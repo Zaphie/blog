53022136291208505d14d7982fa49eaf2b4f6ea5
Zaphie <ddnmikbit@gmail.com>
2020-04-24 10:16:14 -0400

    Add gitignore

--- app.js
+++ app.js
@@ -66,7 +66,11 @@ app.get("/post/:topic", function(req, res){
 
 });
 
+let port = process.env.PORT;
+if ( port == ""){
+  port= 3000;
+}
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen( port, function() {
   console.log("Server started on port 3000");
 });

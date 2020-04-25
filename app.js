3247df47d893b495e52d48f5b8192045bd08270f
Zaphie <ddnmikbit@gmail.com>
2020-04-25 10:02:31 -0400

    Guardando los post en una DB usando mongoose

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = " Tú eres creador de tu vida, y tú puedes construir el camino de una vida más plena y mejor.Tú eres más fuerte que tus miedos. Tus fuerzas son mayores que tus dudas. Aunque tu mente esté confundida, tu corazón siempre sabe la respuesta. Con el tiempo, lo que hoy es difícil mañana será una conquista. Esfuérzate por lo que realmente te llena el alma. Y ten la virtud de saber esperar.";
+const aboutContent = "Es solo un blog, creado como parte de un ejercicio. El resultado es que el usuario pueda escribir un post y subirlos a la web, se guarda en una Base de Datos. Hay más trabajo backEnd que frontEnd";
+const contactContent = "Si eres mi amigo tienes mi contacto. Mua .";
 
 const app = express();
 
@@ -18,15 +19,25 @@ app.use(express.static("public"));
 
 let posts = [];
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
+const postSchema = new mongoose.Schema({
+  title :String,
+  content: String
+});
 
-
+const Post= mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res){
-  res.render("home", {
-    intro: homeStartingContent,
-    posts: posts});
-});
+  Post.find({"title": {$exists:true}, "content": {$exists:true}}, function(err, posts){
+    console.log (posts);
+    res.render("home", {
+      intro: homeStartingContent,
+      posts: posts
+    });
+   });
+  });
+
 
 
 app.get("/about", function(req, res){
@@ -43,34 +54,56 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
-    titleOfPost : req.body.newtitle,
-    contentOfPost: req.body.newContentPost
-  };
-  posts.push(post);
+  const post = new Post ({
+    title : req.body.newtitle,
+    content: req.body.newContentPost
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
+
+  console.log(post.title);
+  console.log(post.content);
+  //posts.push(post);
   res.redirect("/");
 
 });
 
-app.get("/post/:topic", function(req, res){
-  const namePostIntoUserURL = _.lowerCase(req.params.topic);
-  posts.forEach(function(post){
-    var namePost = _.lowerCase(post.titleOfPost);
-    if (namePostIntoUserURL=== namePost){
-      res.render("post",{
-        nameOfTitlePost: namePostIntoUserURL,
-        valueOfContentPost: post.contentOfPost
-      });
-    }
-  });
 
+app.get("/post/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id:requestedPostId}, function(err, post){
+          res.render("post",{
+        title: post.title,
+        content: post.content
+      });
+    });
 });
 
-let port = process.env.PORT;
-if ( port == ""){
-  port= 3000;
-}
 
-app.listen( port, function() {
+
+
+// app.get("/post/:postId", function(req, res){
+//   //   const namePostIntoUserURL = _.lowerCase(req.params.topic);
+//   posts.forEach(function(post){
+//     var namePost = _.lowerCase(post.titleOfPost);
+//     if (namePostIntoUserURL=== namePost){
+//       res.render("post",{
+//         nameOfTitlePost: namePostIntoUserURL,
+//         valueOfContentPost: post.contentOfPost
+//       });
+//     }
+//   });
+//
+// });
+
+// let port = process.env.PORT;
+// if ( port == ""){
+//   port= 3000;
+// }
+
+app.listen( 3000, function() {
   console.log("Server started on port 3000");
 });
